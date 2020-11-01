cc4c963548916211f59f0d4c97d6296ccdc15804
dashaevsina <73135155+dashaevsina@users.noreply.github.com>
2020-11-02 00:04:47 +0100

    Delete Programming exercise 10).py

--- Programming exercise 10).py
+++
@@ -1,7 +0,0 @@
-# This program converts distances measured in kilometres to miles
-
-def main():
-    kilometres = eval(input("What is the distance in kilometres? "))
-    miles = kilometres * .62
-    print ("The distance is", miles, "miles.")
-main()
